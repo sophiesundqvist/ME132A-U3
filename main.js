cacd99bcf1dcabf36d4668f6db0455228a8fe87b
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-03-22 23:35:40 +0100

    Made a function that returns an array of sumPoints for each student based on  searched lastname

--- main.js
+++ main.js
@@ -14,14 +14,61 @@ function filterByLastName (lastname){
 
 
 
+function getArrayOfSumOfPoint(){
+    let lastname = document.getElementById("search").value
+    let students = filterByLastName(lastname)
+    let sumOfPoints = []
+
+    for(let i = 0; i < students.length; i++){
+        let arrayCourses = students[i].courses
+        let sum = 0
+
+            for(let i = 0; i < arrayCourses.length; i++){
+                let points = arrayCourses[0].passedCredits
+
+                sum = sum + points
+            }
+
+        sumOfPoints.push(sum)
+    }
+    
+    return sumOfPoints
+}
+
+
+// function getSumOfPointsBasedOnLastname (lastname){
+//     let sumOfPoints = []
+
+//     for (let i = 0; i < DATABASE.students.length; i++){
+
+//         if (DATABASE.students[i].lastName == lastname){
+//             let arrayCourses = DATABASE.students[i].courses
+//             console.log(arrayCourses)
+//             let sum = 0
+
+//             for (let i = 0; i < arrayCourses.length; i++){
+//                 let points = arrayCourses[0].passedCredits
+
+//                 sum = sum + points
+//             }
+//             sumOfPoints.push(sum)
+//         }
+//     }
+//     return sumOfPoints
+
+// }
+
+
+
+
 // create div with student
-function createDivWithStudent (name, lastname){
+function createDivWithStudent (name, lastname, credit){
     let div = document.createElement("div")
     div.classList.add("box")
     document.getElementById("result").appendChild(div)
 
     div.innerHTML = 
-    `<h3> ${name} ${lastname} (total: credits)</h3>
+    `<h3> ${name} ${lastname} (total: (${credit}credits)</h3>
     <h4> Courses </h4>
     <div class = "courses-result"></div>`
 }
@@ -31,9 +78,10 @@ function createDivWithStudent (name, lastname){
 function setFilterdstudent(){
     let lastname = document.getElementById("search").value
     let students = filterByLastName(lastname)
+    let points = getArrayOfSumOfPoint()
 
     for(let i = 0; i < students.length; i++){
-        createDivWithStudent(students[i].firstName, students[i].lastName, students[i].courses )
+        createDivWithStudent(students[i].firstName, students[i].lastName, points[i]  )
     }
 }
 
