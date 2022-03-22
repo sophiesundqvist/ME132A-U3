acb2f8b15a99d16f782194f1c55754e5835a35d7
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-03-22 16:00:48 +0100

    created functions that create divs(not completed)
    Not completed, just to have an idea of what it could look like

--- main.js
+++ main.js
@@ -14,4 +14,29 @@ function filterByLastName (lastname){
 
 
 
+// create div with student
+function createDivWithStudent (name, lastname){
+    let div = document.createElement("div")
+    div.classList.add("box")
+    document.getElementById("result").appendChild(div)
+
+    div.innerHTML = 
+    `<h3> ${name} ${lastname} (total: credits)</h3>
+    <h4> Courses </h4>
+    <div class = "courses-result"></div>`
+}
+
+
+// creates div based on array of filterd students
+function setFilterdstudent(){
+    let lastname = document.getElementById("search").value
+    let students = filterByLastName(lastname)
+
+    for(let i = 0; i < students.length; i++){
+        createDivWithStudent(students[i].firstName, students[i].lastName, students[i].courses )
+    }
+}
+
+setFilterdstudent()
+
 
