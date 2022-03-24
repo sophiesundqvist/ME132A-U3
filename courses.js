f3bee0d7c4c71c0e32701587c8745b98bded78c6
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-03-24 20:49:44 +0100

    made 2 function to create html with courses

--- courses.js
+++ courses.js
@@ -3,5 +3,38 @@ function filterCoursesByTitle(coursetitle){
         return course.title.toLowerCase().includes(coursetitle)
     })
 
-    return courses
 }
+
+
+function cretaDiv (title, credits){
+
+    let courseDiv = document.createElement("div")
+    courseDiv.classList.add("box")
+    courseDiv.innerHTML = `
+    <h3>${title} (Credits ${credits})</h3>
+    <h4>Courses</h4>`
+
+    return courseDiv
+}
+
+function setCourseHTML (){
+    let courseTitle = document.getElementById("search").value
+    let courses = filterCoursesByTitle(courseTitle)
+    let wrapper = document.getElementById("result")
+
+    // tömmer wrapper på divar innan loopen körs
+    wrapper.innerHTML=""
+    for (let course of courses){
+        let courseDiv = cretaDiv(course.title, course.totalCredits)
+        wrapper.appendChild(courseDiv)
+    }
+}
+
+
+// lystnar på knapptryck i sökrutan
+function setEventListener (){
+    let inputfield = document.getElementById("search")
+    inputfield.addEventListener("keyup", setCourseHTML)
+}
+
+setEventListener()
