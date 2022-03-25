70a77ffe50a9c79b62e2ba0365039002fc3a43dd
sophiesundqvist <sophie.sundqvist@hotmail.com>
2022-03-25 09:43:18 +0100

    made a function that get me an array with arrays of students that  been in each course

--- courses.js
+++ courses.js
@@ -12,7 +12,7 @@ function cretaDiv (title, credits){
     courseDiv.classList.add("box")
     courseDiv.innerHTML = `
     <h3>${title} (Credits ${credits})</h3>
-    <h4>Courses</h4>`
+    <h4>Students</h4>`
 
     return courseDiv
 }
@@ -38,3 +38,24 @@ function setEventListener (){
 }
 
 setEventListener()
+
+
+// går igenom kurserna som är filtrerade
+// returnerar en array genom map som =>
+// går sedan igenom DATABASE.students och filtrerar ut dvs returnera en array med de studenter =>
+// som stämmer överens med student.courses.find =>
+// som sedan stämmer överens med och har samma kurs id som titelns kursid
+// 
+// functionen returnera en array med så många "celler" som det finns kurser =>
+//  i de cellerna är det en array med så många studenter som gått kurserna
+function getStudentByCourseID (coursetitle){
+    let courses = filterCoursesByTitle(coursetitle) 
+
+    return courses.map(course => {
+        return DATABASE.students.filter(student=>{
+            return student.courses.find(studentCourse=>{
+                return studentCourse.courseId == course.courseId
+            })
+        })
+    })
+}
